a0d3ba2156d0729ce96420740738d030d560042e
Deyan Totev <deyan.totev.extern@seven.one>
2023-12-17 19:40:41 +0200

    chore@small

--- src/editordecoration/get-color.ts
+++ src/editordecoration/get-color.ts
@@ -1,4 +1,11 @@
-const colors = [
+import { window, ColorThemeKind } from "vscode";
+
+function isDarkTheme() {
+  const currentTheme = window.activeColorTheme.kind;
+  return currentTheme === ColorThemeKind.Dark;
+}
+
+const colorsLight = [
   "#00ff00",
   "#0bf300",
   "#16e800",
@@ -24,6 +31,35 @@ const colors = [
   "#f30b00",
   "#fe0000"
 ]
+
+const colorsDark = [
+  "#6ee7b7",
+  "#74e2b7",
+  "#7addb8",
+  "#80d8b8",
+  "#87d3b9",
+  "#8dcfba",
+  "#93caba",
+  "#9ac5bb",
+  "#a0c0bc",
+  "#a6bbbc",
+  "#adb7bd",
+  "#b3b2be",
+  "#b9adbe",
+  "#bfa8bf",
+  "#c6a4c0",
+  "#cc9fc0",
+  "#d29ac1",
+  "#d995c2",
+  "#df90c2",
+  "#e58cc3",
+  "#ec87c4",
+  "#f282c4",
+  "#f87dc5",
+  "#fe79c6"
+]
+
+const colors = isDarkTheme() ? colorsDark : colorsLight
 
 export function getColor(complexity: number): string {
   return colors[complexity] ?? '#f816f2'
