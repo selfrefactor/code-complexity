184c1ac30902013db700d97f1beaf44b95f069b6
Deyan Totev <deyan.totev.extern@seven.one>
2023-12-18 19:02:42 +0200

    chore@small

--- src/editordecoration/get-color.ts
+++ src/editordecoration/get-color.ts
@@ -1,75 +1,106 @@
-import { window, ColorThemeKind } from "vscode";
+import {window, ColorThemeKind} from 'vscode'
 
 function isDarkTheme() {
-  const currentTheme = window.activeColorTheme.kind;
-  return currentTheme === ColorThemeKind.Dark;
+  const currentTheme = window.activeColorTheme.kind
+  return currentTheme === ColorThemeKind.Dark
 }
 
-const colorsLight = [
-  "#00ff00",
-  "#08f600",
-  "#11ed00",
-  "#1ae400",
-  "#23db00",
-  "#2bd300",
-  "#34ca00",
-  "#3dc100",
-  "#46b800",
-  "#4faf00",
-  "#57a700",
-  "#609e00",
-  "#699500",
-  "#728c00",
-  "#7b8300",
-  "#837b00",
-  "#8c7200",
-  "#956900",
-  "#9e6000",
-  "#a75700",
-  "#af4f00",
-  "#b84600",
-  "#c13d00",
-  "#ca3400",
-  "#d32b00",
-  "#db2300",
-  "#e41a00",
-  "#ed1100",
-  "#f60800",
-  "#fe0000"
-]
+const LEVEL1 = {
+  light: [
+    '#c3fec3',
+    '#b1feb1',
+    '#9ffe9f',
+    '#8dfe8d',
+    '#7cfe7c',
+    '#6afe6a',
+    '#58fe58',
+    '#46fe46',
+    '#35fe35',
+    '#23fe23',
+    '#11fe11',
+    '#00ff00',
+  ],
+  dark: [
+    '#c3fec3',
+    '#b1feb1',
+    '#9ffe9f',
+    '#8dfe8d',
+    '#7cfe7c',
+    '#6afe6a',
+    '#58fe58',
+    '#46fe46',
+    '#35fe35',
+    '#23fe23',
+    '#11fe11',
+    '#00ff00',
+  ],
+}
 
-const colorsDark = [
-  "#6ee7b7",
-  "#73e3b7",
-  "#78dfb8",
-  "#7ddbb8",
-  "#82d7b9",
-  "#87d4b9",
-  "#8cd0ba",
-  "#91ccba",
-  "#96c8bb",
-  "#9bc4bb",
-  "#a0c1bc",
-  "#a5bdbc",
-  "#aab9bd",
-  "#afb5bd",
-  "#b4b1be",
-  "#b9aebe",
-  "#beaabf",
-  "#c3a6bf",
-  "#c8a2c0",
-  "#cd9ec0",
-  "#d29bc1",
-  "#d797c1",
-  "#db93c2",
-  "#e08fc2",
-  "#e58bc3",
-  "#ea88c3",
-  "#ef84c4",
-  "#f480c4",
-  "#f97cc5",
-  "#fe79c6"
-]
+const LEVEL2 = {
+  light: [
+    '#a1defd',
+    '#92c9fd',
+    '#83b5fd',
+    '#75a1fd',
+    '#668dfd',
+    '#5779fd',
+    '#4964fe',
+    '#3a50fe',
+    '#2b3cfe',
+    '#1d28fe',
+    '#0e14fe',
+    '#0000ff',
+  ],
+  dark: [
+    '#a1defd',
+    '#92c9fd',
+    '#83b5fd',
+    '#75a1fd',
+    '#668dfd',
+    '#5779fd',
+    '#4964fe',
+    '#3a50fe',
+    '#2b3cfe',
+    '#1d28fe',
+    '#0e14fe',
+    '#0000ff',
+  ],
+}
+
+const LEVEL3 = {
+  light: [
+    '#fb9bbe',
+    '#fb8cac',
+    '#fb7e9b',
+    '#fc708a',
+    '#fc6278',
+    '#fc5467',
+    '#fd4656',
+    '#fd3845',
+    '#fd2a33',
+    '#fe1c22',
+    '#fe0e11',
+    '#ff0000',
+  ],
+  dark: [
+    '#fb9bbe',
+    '#fb8cac',
+    '#fb7e9b',
+    '#fc708a',
+    '#fc6278',
+    '#fc5467',
+    '#fd4656',
+    '#fd3845',
+    '#fd2a33',
+    '#fe1c22',
+    '#fe0e11',
+    '#ff0000',
+  ],
+}
+
+const colorsLight = [...LEVEL1.light, ...LEVEL2.light, ...LEVEL3.light]
+
+const colorsDark = [...LEVEL1.dark, ...LEVEL2.dark, ...LEVEL3.dark]
 
 const colors = isDarkTheme() ? colorsDark : colorsLight
 
